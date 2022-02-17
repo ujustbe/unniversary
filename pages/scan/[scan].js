d3a4a72f0bc998e591e5351855b6bed38b96538c
soni-ujustbe <88655561+soni-ujustbe@users.noreply.github.com>
2022-02-17 12:55:21 +0530

    scann module delete

--- pages/scan/[scan].js
+++ pages/scan/[scan].js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 import QRCode from 'qrcode'
 import Link from 'next/link'
 import axios from 'axios';
